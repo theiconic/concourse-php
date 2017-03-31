48e025bf4f702bc3b881929d7c8e9ce972d26696
Manuel Silva <manuel.silva@theiconic.com.au>
2017-04-01 01:08:20 +1100

    Testing concourse

--- pipeline/test.sh
+++ pipeline/test.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 cd built-concourse-php
-php vendor/bin/phpunit --log-junit test-results/results.xml --testdox-text test-results/testdox.txt
+php vendor/bin/phpunit --log-junit ../test-results/results.xml --testdox-text ../test-results/testdox.txt
